8c589e3c7579fabb81afe0f6d5f55ed207f02175
17murph <cmurphy@sjmalaw.com>
2025-03-20 14:05:49 -0400

    Fix: Added default export to static-page.tsx

---
+++ app/blog/static-page.tsx
@@ -0,0 +1,9 @@
+export default function StaticPage() {
+    return (
+      <div>
+        <h1>Static Page</h1>
+        <p>This is a placeholder for the static blog page.</p>
+      </div>
+    );
+  }
+  
